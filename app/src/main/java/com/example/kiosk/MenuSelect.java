b68a5b124d3e1275869ddb7eb13f25461c116195
SMLEE <smlee8806@naver.com>
2020-11-22 19:02:05 +0900

    first commit

---
+++ app/src/main/java/com/example/kiosk/MenuSelect.java
@@ -0,0 +1,14 @@
+package com.example.kiosk;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MenuSelect extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_select);
+    }
+}
